55c323c039fa1fca4e7727cc8cdab70ec8bf921d
AalishMS <aalishmansingh@gmail.com>
2026-09-16 13:10:26 +0545

    Bust Peakendra image cache

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ const characters = [
   {
     name: "Peakendra",
     displayName: "Absolute Peakendra",
-    image: "/assets/peakendra.jpg",
+    image: "/assets/peakendra-v2.jpg",
     alt: "Peakendra, a person holding back tears because it's so peak",
   },
 ];
